9c17c0fb589f13579bfa6f23a7cdc301669e1446
Cory Redmond <cory.redmond@nandos.co.uk>
2021-11-04 10:41:12 +0000

    NAAAT

--- inoffice/messages.go
+++ inoffice/messages.go
@@ -32,7 +32,7 @@ func BuildInOfficeMessage(weekStart time.Time, o map[Day][]InOffice) slack.Messa
 
 	// Truncate nil blocks, blergh
 	for s, v := range blocks {
-		if val := reflect.ValueOf(v); val.IsValid() && val.Interface() != nil && val.IsNil() {
+		if val := reflect.ValueOf(v); val.IsValid() && val.Interface() != nil && !val.IsNil() {
 			blocks = append(blocks[:s], blocks[s+1:]...)
 		}
 	}
